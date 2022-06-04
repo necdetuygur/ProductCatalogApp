8590faf9c485dd47f2c21083dd0d6dd919871f22
Necdet UYGUR <necdet.uygur@gmail.com>
2022-06-04 14:35:46 +0300

    MySentOffers ayrı component yapıldı.

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router";
-import { withdrawOffer } from "../actions";
+import MySentOffers from "./MySentOffers";
 
 function MyAccount(props) {
-  const [currentTab, setCurrentTab] = React.useState(1);
   let navigate = useNavigate();
+  const [currentTab, setCurrentTab] = React.useState(1);
   React.useEffect(() => {
     !props.token && navigate("/");
     // eslint-disable-next-line
@@ -49,52 +49,7 @@ function MyAccount(props) {
       </div>
       <div className="card-body p-0">
         {currentTab === 1 && <div>Birinci Sekme</div>}
-        {currentTab === 2 && (
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th>{props.language.productName}</th>
-                <th>{props.language.offerPrice}</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {props.mySentOffers.map((order) => (
-                <tr key={order.id}>
-                  <td>
-                    {
-                      props.products.find(
-                        (x) => x.id + "" === order.productId + ""
-                      ).name
-                    }
-                  </td>
-                  <td>
-                    {order.price}
-                    {props.language.priceSign}
-                  </td>
-                  <td className="text-end">
-                    <button
-                      className="btn btn-primary btn-sm ms-1"
-                      onClick={() => {
-                        navigate("/product/" + order.productId);
-                      }}
-                    >
-                      {props.language.showProduct}
-                    </button>
-                    <button
-                      className="btn btn-danger btn-sm ms-1"
-                      onClick={() => {
-                        props.withdrawOffer(order.id);
-                      }}
-                    >
-                      {props.language.withdrawOffer}
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
+        {currentTab === 2 && <MySentOffers />}
         {currentTab === 3 && <div>Üçüncü Sekme</div>}
       </div>
     </div>
@@ -105,9 +60,6 @@ export default connect(
   (state) => ({
     language: state.language,
     token: state.token,
-    mySentOffers: state.mySentOffers,
-    products: state.products,
-    withdrawOfferSuccess: state.withdrawOfferSuccess,
   }),
-  { withdrawOffer }
+  {}
 )(MyAccount);
---
+++ src/components/MySentOffers.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router";
+import { withdrawOffer } from "../actions";
+
+function MySentOffers(props) {
+  let navigate = useNavigate();
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>{props.language.productName}</th>
+          <th>{props.language.offerPrice}</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.mySentOffers.map((order) => (
+          <tr key={order.id}>
+            <td>
+              {
+                props.products.find((x) => x.id + "" === order.productId + "")
+                  .name
+              }
+            </td>
+            <td>
+              {order.price}
+              {props.language.priceSign}
+            </td>
+            <td className="text-end">
+              <button
+                className="btn btn-primary btn-sm ms-1"
+                onClick={() => {
+                  navigate("/product/" + order.productId);
+                }}
+              >
+                {props.language.showProduct}
+              </button>
+              <button
+                className="btn btn-danger btn-sm ms-1"
+                onClick={() => {
+                  props.withdrawOffer(order.id);
+                }}
+              >
+                {props.language.withdrawOffer}
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default connect(
+  (state) => ({
+    language: state.language,
+    token: state.token,
+    mySentOffers: state.mySentOffers,
+    products: state.products,
+    withdrawOfferSuccess: state.withdrawOfferSuccess,
+  }),
+  { withdrawOffer }
+)(MySentOffers);
