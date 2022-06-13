f636a14d49514dd739eb796027e7ea8e4a1cd1f3
Necdet UYGUR <necdet.uygur@gmail.com>
2022-06-13 21:04:13 +0300

    Ürünün herhangi bir yerine tıklandığında, ürün detay sayfasına yönlendirme yapıldı.

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,23 +7,20 @@ function Product(props: any) {
   let navigate = useNavigate();
 
   return (
-    <div className="col-md-3 mb-3">
-      <div className="card">
+    <div
+      className="col-md-3 mb-3"
+      onClick={() => {
+        navigate("/product/" + props.product.id);
+      }}
+    >
+      <div className="card bg-light">
         <img
           style={{ minWidth: "100%", height: "150px" }}
           className="card-img-top"
           src={config.PICTURE_BASE + "/" + props.product.picture}
           alt=""
-          onClick={() => {
-            navigate("/product/" + props.product.id);
-          }}
         />
-        <div
-          className="card-body"
-          onClick={() => {
-            navigate("/product/" + props.product.id);
-          }}
-        >
+        <div className="card-body">
           <h5 className="card-title">{props.product.name}</h5>
           <div className="card-text">{props.product.description}</div>
         </div>
